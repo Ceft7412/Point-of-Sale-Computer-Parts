60fc060a48bdf5cf85e322b91ec2d959973d11bb
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-05-25 07:21:09 +0800

    updated

--- config/mail.php
+++ config/mail.php
@@ -46,7 +46,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
-            'verify_peer'       => false,
+            'verify_peer' => false,
         ],
 
         'ses' => [
@@ -105,7 +105,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'cedrick.caceres741@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'example@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Hello'),
     ],
 
